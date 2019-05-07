bc2e573bb793b7d157c9dcdb04481a11f3f5225d
Dustin Haefele <Dustin.haefele@gmail.com>
2019-05-07 18:26:40 -0400

    built out URL and recieved data from API and put it in store

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+/*global $*/
+
 'use strict'; 
 
-const apiKey = 'HGsppNbmfMhyYsR0zjQYwhhsQp4ElEfIcmshvRvE';
-const searchURL ='https://developer.nps.gov/api/v1/parks';
+const store = {
+  api_key: 'HGsppNbmfMhyYsR0zjQYwhhsQp4ElEfIcmshvRvE',
+  searchURL: 'https://developer.nps.gov/api/v1/parks',
+  stateCode: '',
+  results: [],
+  limit: 10,
+};
+
+
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
@@ -9,15 +18,61 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
 
+// function createParksList() {
+//   store.resul
+// }
+
+function buildResultString {
+  
+}
+
 function displayResult() {
-
+  
 }
 
-function buildFullURL(stateSearch, maxResults = 10) {
+function buildFullURL() {
   const params ={
-    api_key: apiKey,
-    stateCode: stateSearch,
-    
-  }
+    api_key: store.api_key,
+    stateCode: store.stateCode,
+    limit: store.limit,
+  };
 
+  const queryParams = formatQueryParams(params);
+  const url = `${store.searchURL}?${queryParams}`;
+
+  return url;
 }
+
+function getParks(){
+  fetch(buildFullURL())
+    .then(response => response.json())
+    .then(jsonResponse => store.results = jsonResponse.data);
+    //.catch()
+}
+
+function setLimit(limit){
+  store.limit = limit - 1;
+}
+
+function setStateCode(stateCode){
+  store.stateCode = stateCode;
+}
+
+function handleFormSubmit() {
+  $('#parks-search-form').submit((e) => {
+    e.preventDefault();
+    const stateCode = $('#state-search').val();
+    const limit = $('#max-results').val();
+    setStateCode(stateCode);
+    setLimit(limit);
+    getParks();
+  });
+}
+
+function main() {
+  handleFormSubmit();
+}
+
+
+$(main);
+
